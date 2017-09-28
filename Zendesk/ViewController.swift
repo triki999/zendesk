1d0da4e50d52331ce506aea5d3c51e41b35a14a2
andrei <andrei@andreis-Mac.local>
2017-09-28 21:26:07 +0300

    Initial Commit

---
+++ Zendesk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Zendesk
+//
+//  Created by andrei on 9/28/17.
+//  Copyright © 2017 andrei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
